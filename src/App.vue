eb0de05a7f052c1598a5c5ffbd9d109a4f187cc6
Emma <50114728+isErya@users.noreply.github.com>
2023-03-21 22:53:48 -0400

    Map scaling refactor

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
     <header v-bind:class="{ 'homePageLogo': $route.path == '/' }">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <img alt="Vacansee logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
       <div class="wrapper">
         <nav>
