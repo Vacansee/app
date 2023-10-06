3d53d050e168b08b86e4ee18f64e3788fd219588
Bren <94324138+Acumane@users.noreply.github.com>
2023-10-06 16:42:36 -0400

    Click-thru navbar
    soltn to pull/25

--- src/App.vue
+++ src/App.vue
@@ -48,24 +48,22 @@ export default {
 
 header {
   z-index: 7;
-  line-height: 1.5;
   display: flex;
-  padding-right: calc(var(--section-gap) / 2);
-  margin: 0 auto;
-  padding: 2rem;
-  font-weight: normal;
+  padding: 1rem;
+  pointer-events: none;
 }
 
 .logo {
   display: block;
-  margin: 0 2rem 0 0;
+  margin: 0 1rem 0 0;
 }
 
 nav {
-  width: 100%;
-  text-align: left;
+  line-height: 2;
   font-size: 1rem;
-  padding: 1rem 0;
+  text-align: left;
+  pointer-events: all;
+  margin: 1.25rem;
 }
 
 nav a.router-link-exact-active {
