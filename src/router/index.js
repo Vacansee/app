85548d53d8876abdcdc99959f479440ea0df9d0d
Josh Parent <joshuamparent@gmail.com>
2024-02-25 17:50:42 -0500

    Worked on the feature that allows a user to manually enter a URL path  (not working)

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,10 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import { inject } from 'vue';
 // import About from '../views/About.vue' // currently unused, could be a settings page
+
+const global = inject('global');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -39,6 +42,35 @@ const router = createRouter({
       redirect: '/' 
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  const pathComponents = to.path.split('/').filter(component => component !== '');
+  const pathBuilding = pathComponents[0]
+  const pathFloorOrRooms = pathComponents[1]
+
+  console.log(pathBuilding, pathFloorOrRooms, global.bldg)
+  
+  if (pathBuilding != "get global building implemented in file") {
+    if (pathFloorOrRooms <= 7) {
+      console.log("Floor path entered")
+      if (pathFloorOrRooms != "global floor value") {
+        // Navigate to requested floor
+      }
+    }
+    else {
+      console.log("Room path entered")
+      if (pathFloorOrRooms != "global room value") {
+        // Navigate to requested room
+      }
+    }
+  }
+
+  // console.log(to.path)
+  // console.log(global.room)
+  // console.log(1 == 1)
+  
+  next();
 });
 
 export default router
